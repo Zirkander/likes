fa0c53441f356805085a6fa921c2c6af1cf4f2ad
Zirkander <lawrence@lawrencecox.net>
2021-05-26 20:23:36 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var clicksNM = 0;
+
+function addLikeNM(){
+    clicksNM += 1;
+    document.getElementById("clicksNM").innerHTML = clicksNM;
+}
+
+var clicksNK = 0;
+
+function addLikeNK(){
+    clicksNK += 1;
+    document.getElementById("clicksNK").innerHTML = clicksNK;
+}
+
+var clicksJR = 0;
+
+function addLikeJR(){
+    clicksJR += 1;
+    document.getElementById("clicksJR").innerHTML = clicksJR;
+}
